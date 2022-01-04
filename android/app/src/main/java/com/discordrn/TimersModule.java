93b0f79cdddf8b9fa7aae0dfefa9db21b0e3acf6
js6pak <kubastaron@hotmail.com>
2022-01-04 22:47:39 +0100

    Fix TimersModule, maybe?

--- android/app/src/main/java/com/discordrn/TimersModule.java
+++ android/app/src/main/java/com/discordrn/TimersModule.java
@@ -1,46 +1,58 @@
 package com.discordrn; // replace com.your-app-name with your app’s name
-import android.os.Handler;
 
+import android.os.Handler;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.modules.core.TimingModule;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
-import java.util.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class TimersModule extends ReactContextBaseJavaModule {
+    private static final String TAG = "TimersModule";
+
     TimersModule(ReactApplicationContext context) {
-       super(context);
-   }
+        super(context);
+    }
 
-   @Override
-   public String getName() {
-       return "TimersModule";
-   }
+    @NonNull
+    @Override
+    public String getName() {
+        return "TimersModule";
+    }
 
-   @ReactMethod
-   public void setTimeout(Callback cb, int t) {
-        new Handler().postDelayed(cb::invoke, t);
-   }
-   @ReactMethod
-   public void setInterval(Callback cb, int t) {
-       new Timer().scheduleAtFixedRate(new TimerTask() {
-          @Override
-          public void run() {
-             cb.invoke();
-          }
-       }, 0, t);
-   }
-   @ReactMethod
-   public void clearTimeout(int t) {}
+    @ReactMethod
+    public void setTimeout(Callback callback, int delay) {
+        Log.i(TAG, "setTimeout: " + delay);
+        new Handler().postDelayed(callback::invoke, delay);
+    }
 
-   @Override
-   public Map<String, Object> getConstants() {
-      final Map<String, Object> constants = new HashMap<>();
-    //   constants.put("", "");
-      return constants;
-   }
+    @ReactMethod
+    public void setInterval(int delay, int period) {
+        Log.i(TAG, String.format("setInterval: delay %d, period %d", delay, period));
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                WritableMap map = Arguments.createMap();
+                map.putInt("id", 69); // TODO
+
+                getReactApplicationContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("interval", map); // or "timer"? idk
+            }
+        }, delay, period);
+    }
+
+    @ReactMethod
+    public void clearTimeout(int id) {
+        Log.i(TAG, "clearTimeout: " + id);
+    }
 }
